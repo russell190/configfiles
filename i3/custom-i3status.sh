78f62e40c24582b1d922539eebdfe5abd8a61741
root <russelljacobs111@gmail.com>
2017-05-04 23:47:58 -0500

    Prettied up i3-config

--- i3/custom-i3status.sh
+++ i3/custom-i3status.sh
@@ -8,19 +8,17 @@ do
     mpd_song=$(mpc current)
 weather_shit=$(/home/rjacobs/i3-weather/weather.py 2402731)
 
-
-
-    if [ "$spotify_status" = "Playing" ] ; then
-        echo -n "NP: $spotify_artist - $spotify_song | $line" || exit 
-        continue
-    elif [ "$mpd_song" != "" ]; then
+    if [ "$mpd_song" != "" ]; then
         mpd_status=$(mpc status | tail -2 | head -1 | cut -d' ' -f1 | tr -d '[]')
         if [ "$mpd_status" = "playing" ]; then            
-            echo -n "  $mpd_song | $line |  $weather_shit" || exit 1
+            echo -n "  $mpd_song | $line |  $weather_shit"  || exit 
             continue
         fi
+	sleep 5m
         echo -n "$line |  $weather_shit" || exit 1
         continue
     fi
-    echo -n "$line |  $weather_shit" || exit 1
+    echo -n "$line |  $weather_shit" || 
+sleep 5m exit 1
+
 done
