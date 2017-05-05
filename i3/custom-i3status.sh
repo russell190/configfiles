bb1cd885ec0f44d3a5f5a950a461aa3e67516e44
root <russelljacobs111@gmail.com>
2017-05-05 00:07:38 -0500

    Added slight delay after while loop to prevent weather script from updating continuously

--- i3/custom-i3status.sh
+++ i3/custom-i3status.sh
@@ -10,13 +10,15 @@ weather_shit=$(/home/rjacobs/i3-weather/weather.py 2402731)
 
     if [ "$mpd_song" != "" ]; then
         mpd_status=$(mpc status | tail -2 | head -1 | cut -d' ' -f1 | tr -d '[]')
-else
         if [ "$mpd_status" = "playing" ]; then            
             echo -n "  $mpd_song | $line |  $weather_shit"  || exit 1
             continue
-        else
         echo -n "$line |  $weather_shit" || exit 1
         continue
     fi
-sleep 5m
+    echo -n "$line |  $weather_shit" || 
+exit 1
+fi
+sleep 10
+
 done
