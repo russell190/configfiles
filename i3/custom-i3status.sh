d67b2e6280f363bdd8d82f1ec83f4b4a7128fa4f
root <russelljacobs111@gmail.com>
2017-05-06 00:49:07 -0500

    I literally called an if statement within an if statement and therefore nullified the FIRST if statement. This is fixed now

--- i3/custom-i3status.sh
+++ i3/custom-i3status.sh
@@ -11,7 +11,7 @@ weather_shit=$(/home/rjacobs/i3-weather/weather.py 2402731)
 
     if [ "$mpd_song" != "" ]; then
         mpd_status=$(mpc status | tail -2 | head -1 | cut -d' ' -f1 | tr -d '[]')
-        if [ "$mpd_status" = "playing" ]; then            
+      else if [ "$mpd_status" = "playing" ]; then            
             echo -n "  $mpd_song | $line |  $weather_shit"  || exit 1
             continue
         echo -n "$line |  $weather_shit" || exit 1
