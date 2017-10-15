8c0eae992e7c8bbc34c97c595a211d2412b6a47f
russell190 <russelljacobs111@gmail.com>
2017-10-15 17:59:59 -0400

    Piped mpc status through sed to impute more reserved chars
    Turns out & isn't the only reserved character that can break i3's status text... changes should be pretty self-explanatory

--- i3/custom-i3status.sh
+++ i3/custom-i3status.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
-weather=$(~/i3-weather/weather.py 2402731)
+weather=$(~/i3-weather/weather.py 2483553)
 
 i3status -c ~/.i3/i3status.conf | while :
 do
     read line
 
     dir=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
-    mpd_song=$(mpc current)
+    mpd_song=$(mpc current | sed 's?[<>*]?_?g')
     mpd_song=${mpd_song// & / and }
 
 
     if [ "$mpd_song" != "" ]; then
-        mpd_status=$(mpc status | tail -2 | head -1 | cut -d' ' -f1 | tr -d '[]')
+        mpd_status=$(mpc status  | tail -2 | head -1 | cut -d' ' -f1 | tr -d '[]')
       if [ "$mpd_status" = "playing" ]; then            
-            echo -n "  $mpd_song | $line |  $weather" |sed 's?|?//?g'|| exit 1
+            echo -n "  $mpd_song | $line |   $weather" |sed 's?|?//?g'|| exit 1
             continue
 	else
-        echo -n "  Paused | $line |  $weather" | sed 's?|?//?g'|| exit 1
+        echo -n "  Paused | $line |   $weather" | sed 's?|?//?g'|| exit 1
         continue
 fi
 fi
-echo -n "  Paused | $line |  $weather" | sed 's?|?//?g'|| exit 1
+echo -n "  Paused | $line |   $weather" | sed 's?|?//?g'|| exit 1
 done
