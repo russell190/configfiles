7fccfb52014957d1204efb0a439c6267f6c57028
russell190 <russelljacobs111@gmail.com>
2017-11-01 23:52:19 -0400

    Added phone status for i3bar
    Since I currently use kdeconnect for phone notifications via fedora, I wanted a way to be able to see if I have the application running at all times. This will execute on startup regardless, but this is to avoid future headache in case I am killing processes

--- i3/custom-i3status.sh
+++ i3/custom-i3status.sh
@@ -8,17 +8,24 @@ do
     dir=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
     mpd_song=$(mpc current | sed 's?[<>*]?_?g')
     mpd_song=${mpd_song// & / and }
+    phone_status=$(ps -ef | grep "indicator-kdeconnect" | grep -v "grep" | wc -l)
+
+if [ "$phone_status" = 1 ]; then
+phone_text=$(echo "")
+else
+phone_text=$(echo " phone n/a.")
 
 
     if [ "$mpd_song" != "" ]; then
         mpd_status=$(mpc status  | tail -2 | head -1 | cut -d' ' -f1 | tr -d '[]')
       if [ "$mpd_status" = "playing" ]; then            
-            echo -n "  $mpd_song | $line |   $weather" |sed 's?|?//?g'|| exit 1
+            echo -n "  $mpd_song | $line |   $weather |$phone_text" |sed 's?|?//?g'|| exit 1
             continue
 	else
-        echo -n "  Paused | $line |   $weather" | sed 's?|?//?g'|| exit 1
+        echo -n "  Paused | $line |   $weather |$phone_text" | sed 's?|?//?g'|| exit 1
         continue
 fi
 fi
-echo -n "  Paused | $line |   $weather" | sed 's?|?//?g'|| exit 1
+fi
+echo -n "  Paused | $line |   $weather |$phone_text" | sed 's?|?//?g'|| exit 1
 done
